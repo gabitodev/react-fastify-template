e04b5194b7a88c5a5fd9f032db945abc7bf23c93
gabitodev <gabitodev@gmail.com>
2025-05-13 16:58:02 -0400

    refactor: move CSS import below React Router import
    This commit moves the CSS import statement below the React Router import in `packages/client/src/main.tsx`. This change improves code readability by grouping React-related imports together. There is no functional change.

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,9 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { createRouter, RouterProvider } from '@tanstack/react-router';
-import './index.css';
-
 import { routeTree } from './routeTree.gen';
+import './index.css';
 
 const router = createRouter({ routeTree });
 
