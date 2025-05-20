7f9986aaa819bb29548fda9a30429c9324a25b6b
Gabriel Garcia <gabitodev@gmail.com>
2025-05-20 14:56:36 -0400

    feat: improved logger in dev and staging enviroments

--- apps/server/index.ts
+++ apps/server/index.ts
@@ -1,7 +1,24 @@
 import Fastify from 'fastify';
 
+const pinoSettings = {
+  transport: {
+    target: 'pino-pretty',
+    options: {
+      translateTime: 'HH:MM:ss Z',
+      ignore: 'pid,hostname',
+    },
+  },
+};
+
+const envToLogger = {
+  development: pinoSettings,
+  staging: pinoSettings,
+  production: true,
+  test: false,
+};
+
 const fastify = Fastify({
-  logger: true,
+  logger: envToLogger[process.env.NODE_ENV as keyof typeof envToLogger] ?? true,
 });
 
 fastify.get('/', function (_request, reply) {
