84248c72b2fe9ad147e8f565ae4d17930d769773
gabitodev <gabitodev@gmail.com>
2025-06-19 22:31:57 -0400

    Refactor code structure for improved readability and maintainability

--- apps/client/eslint.config.js
+++ apps/client/eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
+import eslintReact from '@eslint-react/eslint-plugin';
 
 export default tseslint.config(
   {
@@ -14,15 +15,16 @@ export default tseslint.config(
       js.configs.recommended,
       ...tseslint.configs.recommendedTypeChecked,
       ...tseslint.configs.stylisticTypeChecked,
+      eslintReact.configs['recommended-typescript'],
       eslintPluginPrettier,
     ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       parserOptions: {
-        projectService: true,
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
         tsconfigRootDir: import.meta.dirname,
       },
-      ecmaVersion: 2020,
+      ecmaVersion: 2022,
       globals: globals.browser,
     },
     plugins: {
